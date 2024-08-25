1df93adabcb8dc3cc5457247c46b4f0d4ba9aa33
Muhammad Taha Khan <mtkinverse@gmaile.com>
2024-08-25 22:55:26 +0500

    Posting first version

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        bgTheme:{
+          'light':'#799ebe',
+          'dark':'#205187',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
